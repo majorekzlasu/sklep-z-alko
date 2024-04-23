4933c9c83472fb5550de4ec9b807e546e69f1455
maciusmajor <maciej.kaczmarski@infotech.edu.pl>
2024-04-23 20:50:36 +0200

    Sklep

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -5,6 +5,8 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
     def __str__(self):
         return self.name
+    class Meta:
+        verbose_name_plural = 'categories'
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
@@ -22,6 +24,8 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE,default=1)
     description = models.CharField(max_length=250, default='', null=True, blank=True)
     image = models.ImageField(upload_to='uploads/products/')
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
     def __str__(self):
         return self.name
 
--- ecom/store/urls.py
+++ ecom/store/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
 
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('admin/', admin.site.urls),
 ]
